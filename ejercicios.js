017fa958fbee2080a67ef175a4139dab3af6ff6d
laura jofre <laurajofre66@gmail.com>
2026-05-13 22:44:21 -0400

    first commit

---
+++ ejercicios.js
@@ -0,0 +1,88 @@
+///ejercicio numero 1
+
+let nombre = "laura"
+let apellido = "jofre"
+console.log (nombre , apellido);
+
+//ejercicio numero 2
+
+let a = 7
+
+console.log ((a + 5) * 3 )
+
+//ejercicio numero 3
+
+let comidafavorita   = "pollo con papafritas "
+console.log ( comidafavorita );
+
+//ejercicio numero 4
+
+let d = 10
+let e = 3 ;
+
+console.log ( d + e);
+console.log ( d - e);
+console.log ( d * e);
+console.log ( d % e);
+
+console.log(typeof(d) + e );
+//console.log (number + e );
+
+//ejercicio numero 5 
+
+let edad = 15 
+if (edad >= 10 ){
+console.log (15)
+}
+
+if (edad <= 18 ){
+console.log (15)
+}
+
+//ejercicios numero 6
+let numero = 8 
+if (numero >= 5 && numero <= 10  ){
+console.log (numero)
+}
+
+//ejercios numero 7 
+let nota = 6 
+if (nota == 7 ){
+console.log (nota)
+} 
+
+// ejercio numero 8
+let temperatura = 25
+if (temperatura >= 30 ){
+console.log (temperatura)
+} 
+
+// ejercicios numero 9
+let numer = 8
+let numeros = 9
+if (numer  == numeros ){
+console.log ("los  numeros iguales");
+}
+if (numer > numeros || numer < numeros){
+console.log ("si numero es mayor o menor que numeros");
+} 
+
+// ejercicios numero 10 
+let edades = 17
+let tienepermiso=true
+if(edades >= 18 || tienepermiso){
+console.log ("si es mayor a 18 o ya tiene permiso");
+}
+ 
+ // ejercicios numero 11 
+ let usuario = "admin"
+ let password = "1234"
+ if (usuario == "admin" && password == "1234") {
+console.log (`si ${usuario} es igual a admin y ${password} es igual a 1234 dar la bienvenida`);
+ }
+
+
+
+
+
+ 
---
+++ tarea.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+for (let y = 1 ; y  <= 10 ; y ++){
+
+    console.log(y)
+}
+
+//ejercio numero 2
+for(let y = 1; y <= 20; y++ )
+if (y % 2 == 0){
+console.log (y);
+}else {console.log("");
+
+}
+
+let numero = prompt ("ingresar numero")
+ for(let y = 1; y <= 10; y ++ ){
+console.log ( numero , "*" , y  , "=" , numero * y )
+
+ }
